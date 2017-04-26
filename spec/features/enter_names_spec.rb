282441ffeb13892a49631131218eaf7880e7865a
danielemanuel <daniel.costea@vkernel.ro>
2017-04-26 14:43:03 +0100

    DRY the code b implementing more methods

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -1,21 +1,15 @@
-
+require_relative 'web_helpers'
 
 feature "Enter the player names" do
   scenario "submiting the names" do
-    visit('/')
-    fill_in :player_1_name, with: "Ben"
-    fill_in :player_2_name, with: "Daniel"
-    click_button "Submit"
+    sign_in_and_play
     expect(page).to have_content "Ben vs. Daniel"
   end
 end
 
 feature "View the points" do
   scenario "view secound player points" do
-    visit('/')
-    fill_in :player_1_name, with: 'Ben'
-    fill_in :player_2_name, with: 'Daniel'
-    click_button "Submit"
-    expect(page).to have_content "Daniel: 10 points"
+  sign_in_and_play
+  expect(page).to have_content "Daniel: 10 points"
   end
-end 
+end
