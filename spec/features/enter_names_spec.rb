7142c643792a0c88ab052b9bd3e121bd66ca21d8
danielemanuel <daniel.costea@vkernel.ro>
2017-04-26 14:32:45 +0100

    Adding points to the secound player

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -9,3 +9,13 @@ feature "Enter the player names" do
     expect(page).to have_content "Ben vs. Daniel"
   end
 end
+
+feature "View the points" do
+  scenario "view secound player points" do
+    visit('/')
+    fill_in :player_1_name, with: 'Ben'
+    fill_in :player_2_name, with: 'Daniel'
+    click_button "Submit"
+    expect(page).to have_content "Daniel: 10 points"
+  end
+end 
