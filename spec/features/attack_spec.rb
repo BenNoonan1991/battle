3799c1c63f4f905e024c45da37fe5f2f3b7e10b2
BenNoonan1991 <ben.noonan2@gmail.com>
2017-04-26 16:09:37 +0100

    Initiated attack button

--- spec/features/attack_spec.rb
+++ spec/features/attack_spec.rb
@@ -1,7 +1,7 @@
 feature "Attack" do
   scenario "Attacks second player" do
   sign_in_and_play
-  click_link 'Attack'
+  click_button 'Attack'
   expect(page).to have_content 'Ben attacked Daniel'
-  end 
+  end
 end
