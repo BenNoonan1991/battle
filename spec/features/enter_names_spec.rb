ff64c4a38ec33ca8f4bc6ded7651ac9e8ef591e9
danielemanuel <daniel.costea@vkernel.ro>
2017-04-26 14:51:59 +0100

    Refactoring some code

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -1,15 +1,8 @@
-require_relative 'web_helpers'
+# require_relative 'web_helpers'
 
 feature "Enter the player names" do
   scenario "submiting the names" do
     sign_in_and_play
     expect(page).to have_content "Ben vs. Daniel"
-  end
-end
-
-feature "View the points" do
-  scenario "view secound player points" do
-  sign_in_and_play
-  expect(page).to have_content "Daniel: 10 points"
   end
 end
